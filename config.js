aea7e45482d1ccf7ee63ea8d7bf2e016ed165962
Tuggi <paxtonphillips@outlook.com>
2025-08-07 09:51:29 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 SERVER_URL = 'https://minesweeper-leaderboard.tuggi.dev';
 // SERVER_URL = 'http://localhost:3001';
+LOCAL_STORAGE_USERNAME_KEY = "ms_lb_username";
 LOCAL_STORAGE_COLLAPSE_KEY = "ms_lb_collapsed";

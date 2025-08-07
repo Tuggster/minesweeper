60fa3dab09c8b11aef294638433043fa21ff168a
Tuggi <paxtonphillips@outlook.com>
2025-08-07 00:29:56 -0700

    show date

--- leaderboard.js
+++ leaderboard.js
@@ -66,15 +66,24 @@ const uploadToLeaderboard = async () => {
   showLeaderboard();
 };
 
+function formatDateToYYYYMMDD(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed, so add 1
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
+
 const updateLeaderboard = () => {
   const leaderboardTable = document.querySelector("#leaderboard-data");
   leaderboardTable.innerHTML = "";
-  const labels = ["username", "time (seconds)"];
+  const labels = ["username", "time (seconds)", "date"];
   addEach(leaderboardTable, labels, "th");
 
   leaderboardData.forEach((score) => {
     const style = score.isYou ? 'style="background-color: gold;"' : "";
-    const values = [score.handle, score.time];
+    const time = new Date(score.createdAt);
+    const values = [score.handle, score.time, formatDateToYYYYMMDD(time)];
     addEach(leaderboardTable, values, undefined, style);
   });
 };
